9742c2e66292ddb1e8c57645171dc10a24783117
AyKrimino <ayoub.krimi14@gmail.com>
2025-12-08 19:48:26 +0100

    feat: add markdown preview feature to NoteDetailsActivity

--- app/src/main/java/com/project/notemd/NoteDetailsActivity.java
+++ app/src/main/java/com/project/notemd/NoteDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.project.notemd;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,13 +22,42 @@ public class NoteDetailsActivity extends AppCompatActivity {
         int noteId = getIntent().getIntExtra("note_id", -1);
 
         TextView titleView = findViewById(R.id.noteTitle);
-        TextView contentView = findViewById(R.id.noteContent);
+        WebView markdownView = findViewById(R.id.markdownView);
 
         Note note = notesService.getNoteById(noteId);
 
         if (note != null) {
             titleView.setText(note.getTitle());
-            contentView.setText(note.getContent());
+
+            String html = "<html><head>" +
+                    "<style>" +
+                    "body { font-size: 17px; line-height: 1.6; }" +
+                    "code { background:#eee; padding:4px; border-radius:4px; }" +
+                    "h1,h2,h3 { margin-top:20px; }" +
+                    "</style>" +
+                    "</head><body>" +
+                    markdownToHtml(note.getContent()) +
+                    "</body></html>";
+
+            markdownView.loadDataWithBaseURL(null, html, "text/html", "utf-8", null);
         }
+    }
+
+    private String markdownToHtml(String markdown) {
+        markdown = markdown.replaceAll("(?m)^### (.+)$", "<h3>$1</h3>");
+        markdown = markdown.replaceAll("(?m)^## (.+)$", "<h2>$1</h2>");
+        markdown = markdown.replaceAll("(?m)^# (.+)$", "<h1>$1</h1>");
+
+        markdown = markdown.replaceAll("\\*\\*(.+?)\\*\\*", "<b>$1</b>");
+        markdown = markdown.replaceAll("\\*(.+?)\\*", "<i>$1</i>");
+
+        markdown = markdown.replaceAll("`([^`]+)`", "<code>$1</code>");
+
+        markdown = markdown.replaceAll("(?m)^- (.+)$", "<li>$1</li>");
+        markdown = markdown.replaceAll("(<li>.*</li>)", "<ul>$1</ul>");
+
+        markdown = markdown.replace("\n", "<br>");
+
+        return markdown;
     }
 }
